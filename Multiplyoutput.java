efaffa2810a9034ac1784c56da35148255e81d31
Walter Rada <WaltRCodes@users.noreply.github.com>
2020-04-28 22:48:50 -0400

    adding homework

---
+++ Multiplyoutput.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Multiplyoutput {
+
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        int a = keyboard.nextInt();
+        int b = keyboard.nextInt();
+        System.out.println(a*b);
+
+    }
+}
---
+++ Unitconversion.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Unitconversion {
+
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        int inch = keyboard.nextInt();
+        System.out.println(inch*0.0254);
+
+    }
+}
